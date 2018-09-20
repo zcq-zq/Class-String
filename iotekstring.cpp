548e7e4a62ad124194e697d2b747b4ab2da0ecc3
zcq-zq <43400686+zcq-zq@users.noreply.github.com>
2018-09-20 10:40:40 +0800

    Update iotekstring.cpp
    实现+=，+运算符的重载

--- iotekstring.cpp
+++ iotekstring.cpp
@@ -60,3 +60,38 @@ String&String::operator=(const char* other)
 	}
 	return *this;
 }
+
+String& String::operator+=(const String&other)
+{
+	char*tmp = m_data;
+	int length = strlen(m_data) + strlen(other.m_data);
+	m_data = new char[length + 1];
+	strcpy(m_data, tmp);
+	strcat(m_data, other.m_data);
+	delete[]tmp;
+	return *this;
+}
+
+
+String String::operator+(const String&other) const
+{
+	String result;
+	result += *this;
+	result += other;
+	return result;
+}
+
+
+String& String::operator+=(const char*other)
+{
+	String tmp(other);
+	*this += tmp;
+	return *this;
+}
+	
+String String::operator+(const char*other) const
+{
+	String result = *this;
+	result += other;
+	return result;
+}
